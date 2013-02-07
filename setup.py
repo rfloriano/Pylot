cf76a1bc717a8f5624922f916559358c966d9312
Rafael Floriano da Silva <rflorianobr@gmail.com>
2013-02-07 20:19:28 -0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*-coding: utf-8-*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pylot',
+    version='1.26',
+    description='',
+    author='pmarkert',
+    author_email='rflorianobr@gmail.com',
+    url='https://github.com/rfloriano/Pylot',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+)
